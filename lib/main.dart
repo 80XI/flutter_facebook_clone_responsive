6fee302201abadb33c92fdc0de7d1d938f949dbc
Salil Kayastha <salilkayastha1490@gmail.com>
2022-05-26 12:35:41 +0545

    Basic application folder structure

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_clone_responsive/config/pallette.dart';
+import 'package:flutter_facebook_clone_responsive/screens/screens.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
